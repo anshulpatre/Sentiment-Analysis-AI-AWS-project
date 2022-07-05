9c4f84a1c73eef8969ba49b6f5f35e946f5a995a
souravnk1199 <74104643+souravnk1199@users.noreply.github.com>
2022-07-05 23:47:25 +0530

    Add files via upload

---
+++ flask_app/static/javascript/main_index.js
@@ -0,0 +1,13 @@
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+
+toggleButton.addEventListener("click", () => {
+  navbarLinks.classList.toggle("active");
+});
+<<<<<<< HEAD
+
+function submitForm() {
+  document.getElementById("sentiment").submit();
+}
+=======
+>>>>>>> d9b5f006b0fed786d0dc6498bbfef46b67d189a3
